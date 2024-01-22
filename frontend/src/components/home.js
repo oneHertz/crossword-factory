5cb8b16d99b3bdbf2fcb524a7b18d438d69ea352
Rapha S <rphlo@users.noreply.github.com>
2024-01-22 09:29:50 +0200

    Fr

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -15,7 +15,7 @@ const Home = () => {
         <div className="container main-container">
             <div style={{textAlign:'center'}} >
             {username && <>
-            <Link to="/nouvelle-grille"><button className="btn btn-primary mb-3"><i className="fas fa-plus"></i> + Creer une nouvelle grille</button></Link><br/>
+            <Link to="/nouvelle-grille"><button className="btn btn-primary mb-3"><i className="fas fa-plus"></i> + Créer une nouvelle grille</button></Link><br/>
             </>}
             {!username && <><hr/>
                 <span>Connectez vous pour créer de nouvelles grilles ou voir vos grilles sauvegardées sur le site.</span>
