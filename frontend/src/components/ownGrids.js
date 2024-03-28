b987ab1313fa69ed0abda60d6a66db28d94df189
Rapha S <rphlo@users.noreply.github.com>
2024-03-28 21:30:15 +0200

    Update ownGrids.js

--- frontend/src/components/ownGrids.js
+++ frontend/src/components/ownGrids.js
@@ -41,8 +41,8 @@ function OwnGrids() {
                               e.id +
                               "/preview"
                             } class="card-img-top" alt="Aperçu grille"/>
-                        <h3 style={{width: '100%'}}>{e.published ? <Link className="stretched-link" to={"/grille/"+e.id}>{e.title}</Link> : e.title}
-                        <Link className="float-end" style={{ zIndex: 11 }} to={"/grille/"+e.id+'/modifier'}><button type="button" className="float-right btn btn-primary">Modifier</button></Link></h3>
+                        <h3 style={{width: '100%'}}>{e.published ? <Link className="stretched-link" to={"/grille/"+e.id}>{e.title}</Link> : e.title}</h3>
+                        <div className="float-end" style={{ zIndex: 11 }}><Linkto={"/grille/"+e.id+'/modifier'}><button type="button" className="float-right btn btn-primary">Modifier</button></Link></div>
                         <span>{e.width}x{e.height} {e.published && <span className="badge bg-danger">publié</span>}</span><br/>
                         <span>Créée {(new Date(e.creation_date)).toLocaleDateString("fr-FR")}</span><br/>
                         <span>Modifiée {(new Date(e.modification_date)).toLocaleDateString("fr-FR")}</span>
