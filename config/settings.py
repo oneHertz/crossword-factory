5396747ca1c63c6f6aa47c069628c377a8de32e3
Raphael Stefanini <raphael@rphlo.com>
2025-06-24 15:29:23 +0300

    requirements.in

--- config/settings.py
+++ config/settings.py
@@ -252,10 +252,9 @@ SLUG_BLACKLIST = [
     'maps', 'sign-up', 'password-reset', 'verify-email', 'password-reset-confirmation'
 ]
 
-ACCOUNT_AUTHENTICATION_METHOD = "username"
-ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_LOGIN_METHODS = {'username'}
+ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
 ACCOUNT_USERNAME_BLACKLIST = SLUG_BLACKLIST
-ACCOUNT_USERNAME_REQUIRED = True
 ACCOUNT_USERNAME_MIN_LENGTH = 2
 ACCOUNT_USERNAME_VALIDATORS = 'utils.validators.custom_username_validators'
 ACCOUNT_ADAPTER = 'utils.account_adapter.CustomAccountAdapter'
