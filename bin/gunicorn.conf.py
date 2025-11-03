57bac60375e435bb8165c85b32e892b4682f9fd4
Rapha <rphlo@users.noreply.github.com>
2025-11-03 22:07:28 +0200

    Update gunicorn.conf.py

--- bin/gunicorn.conf.py
+++ bin/gunicorn.conf.py
@@ -2,7 +2,7 @@ wsgi_app = "config.wsgi:application"
 preload_app = True
 daemon = False
 raw_env = ["DJANGO_SETTINGS_MODULE=config.settings"]
-workers = 1
+workers = 2
 threads = 2
 max_requests = 500
 max_requests_jitter = 40
